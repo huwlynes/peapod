2bf94353c404b05b23275105ea8c883eb0853845
huw <huw@2c022bab-3cfb-0310-bff6-e63c505a1314>
2007-06-30 12:12:55 +0000

    update release scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='peapod',
-      version='xxx',
+      version='0.6',
       py_modules=['Peapod.peapod','Peapod.tagging','Peapod.OPML','Peapod.btclient','Peapod.feedparser'],
       scripts=['peapod.py','btclient.py'],
       )
